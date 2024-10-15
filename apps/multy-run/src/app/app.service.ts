4ccc88e9b003b193d7814e6a9b8142c007524f77
Spielcrypto <spieljs@gmail.com>
2024-10-15 14:33:14 +0200

    add example

--- apps/multy-run/src/app/app.service.ts
+++ apps/multy-run/src/app/app.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return ({ message: 'Hello API' });
+    console.log('Getting data');
+    return { message: 'Hello API' };
+  }
+
+  getSecureData(): { message: string } {
+    console.log('Getting secure data');
+    return { message: 'Secure Hello API' };
   }
 }
--- apps/multy-run/src/main.ts
+++ apps/multy-run/src/main.ts
@@ -2,19 +2,23 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { AppService } from './app/app.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  const app = await NestFactory.createApplicationContext(AppModule);
+
+  const appService = app.get(AppService);
+
+  if (process.argv[2] === 'secure') {
+    appService.getSecureData();
+  }
+
+  if (process.argv[2] === 'hello') {
+    appService.getData();
+  }
 }
 
 bootstrap();
